966bf2a37d2993e5d499c339a898afadf615866b
Andrew Richard <acrichard07@gmail.com>
2025-03-07 11:55:31 -0800

    through 2a complete

--- ar-lahman-bonus.sql
+++ ar-lahman-bonus.sql
@@ -1,6 +1,6 @@
 
 -- In these exercises, you'll explore a couple of other advanced features of PostgreSQL. 
-
+--------------------------------------------------------------------------------
 -- 1. In this question, you'll get to practice correlated subqueries and learn about the LATERAL keyword. Note: This could be done using window functions, but we'll do it in a different way in order to revisit correlated subqueries and see another keyword - LATERAL.
 
 -- a. First, write a query utilizing a correlated subquery to find the team with the most wins from each league in 2016.
@@ -53,7 +53,34 @@ WHERE yearid = 2016;
 
 -- c. If you are interested in pulling in the top (or bottom) values by group, you can also use the DISTINCT ON expression (https://www.postgresql.org/docs/9.5/sql-select.html#SQL-DISTINCT). Rewrite your previous query into one which uses DISTINCT ON to return the top team by league in terms of number of wins in 2016. Your query should return the league, the teamid, and the number of wins.
 
+SELECT 
+    DISTINCT ON (lgid) lgid,
+    name,
+    w
+FROM teams t
+WHERE yearid = 2016
+ORDER BY lgid, w DESC;
+
+
 -- d. If we want to pull in more than one column in our correlated subquery, another way to do it is to make use of the LATERAL keyword (https://www.postgresql.org/docs/9.4/queries-table-expressions.html#QUERIES-LATERAL). This allows you to write subqueries in FROM that make reference to columns from previous FROM items. This gives us the flexibility to pull in or calculate multiple columns or multiple rows (or both). Rewrite your previous query using the LATERAL keyword so that your result shows the teamid and number of wins for the team with the most wins from each league in 2016. 
+
+
+
+SELECT *
+FROM (SELECT DISTINCT lgid 
+	  FROM teams
+	  WHERE yearid = 2016) AS leagues,
+	  LATERAL (
+        SELECT name, w
+        FROM teams
+        WHERE yearid = 2016
+            AND lgid = leagues.lgid
+        ORDER BY w DESC
+        LIMIT 1
+       ) as top_teams
+ORDER BY w DESC;
+
+
 
 -- If you want a hint, you can structure your query as follows:
 
@@ -65,7 +92,21 @@ FROM (SELECT DISTINCT lgid
 	  
 -- e. Finally, another advantage of the LATERAL keyword over using correlated subqueries is that you return multiple result rows. (Try to return more than one row in your correlated subquery from above and see what type of error you get). Rewrite your query on the previous problem sot that it returns the top 3 teams from each league in term of number of wins. Show the teamid and number of wins.
 
+SELECT *
+FROM (SELECT DISTINCT lgid 
+	  FROM teams
+	  WHERE yearid = 2016) AS leagues,
+	  LATERAL (
+        SELECT name, w
+        FROM teams
+        WHERE yearid = 2016
+            AND lgid = leagues.lgid
+        ORDER BY w DESC
+        LIMIT 3
+       ) as top_teams
+ORDER BY w DESC;
 
+--------------------------------------------------------------------------------
 -- 2. Another advantage of lateral joins is for when you create calculated columns. In a regular query, when you create a calculated column, you cannot refer it it when you create other calculated columns. This is particularly useful if you want to reuse a calculated column multiple times. For example,
 
 SELECT 
@@ -95,12 +136,22 @@ ORDER BY winning_pct DESC;
 
 -- a. Write a query which, for each player in the player table, assembles their birthyear, birthmonth, and birthday into a single column called birthdate which is of the date type.
 
+SELECT
+    namefirst||' '||namelast AS playername,
+    CAST(birthyear||'-'||birthmonth||'-'||birthday AS TEXT)::DATE AS birthdate
+FROM people AS p 
+GROUP BY playerid;
+
+
+
+
 -- b. Use your previous result inside a subquery using LATERAL to calculate for each player their age at debut and age at retirement. (Hint: It might be useful to check out the PostgreSQL date and time functions https://www.postgresql.org/docs/8.4/functions-datetime.html).
 
 -- c. Who is the youngest player to ever play in the major leagues?
 
 -- d. Who is the oldest player to player in the major leagues? You'll likely have a lot of null values resulting in your age at retirement calculation. Check out the documentation on sorting rows here https://www.postgresql.org/docs/8.3/queries-order.html about how you can change how null values are sorted.
 
+--------------------------------------------------------------------------------
 -- 3. For this question, you will want to make use of RECURSIVE CTEs (see https://www.postgresql.org/docs/13/queries-with.html). The RECURSIVE keyword allows a CTE to refer to its own output. Recursive CTEs are useful for navigating network datasets such as social networks, logistics networks, or employee hierarchies (who manages who and who manages that person). To see an example of the last item, see this tutorial: https://www.postgresqltutorial.com/postgresql-recursive-query/. 
 -- In the next couple of weeks, you'll see how the graph database Neo4j can easily work with such datasets, but for now we'll see how the RECURSIVE keyword can pull it off (in a much less efficient manner) in PostgreSQL. (Hint: You might find it useful to look at this blog post when attempting to answer the following questions: https://data36.com/kevin-bacon-game-recursive-sql/.)
 
